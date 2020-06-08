7dcd3bfd1319a0fe3af04944c8c96f807790bb3f
Devorah Langsam <devorah@langsam.org>
2020-06-07 22:26:51 -0400

    Add add song functionality

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import Song from 'rarwe/models/song';
+import { empty } from '@ember/object/computed';
+
+export default Controller.extend({
+  isAddingSong: false,
+  newSongName: '',
+  isAddButtonDisabled: empty('newSongName'),
+  addSong: action(function() {
+    this.set('isAddingSong', true);
+  }),
+
+  cancelAddSong: action(function() {
+    this.set('isAddingSong', false);
+  }),
+
+  saveSong: action(function(event) {
+    event.preventDefault();
+    let newSong = Song.create({ title: this.newSongName });
+    this.model.songs.pushObject(newSong);
+    this.set('newSongName', '')
+  }),
+
+});
