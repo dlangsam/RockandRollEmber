f92a111e94c7b41cc3bcd875a23ec169e5f93289
Devorah Langsam <devorah@langsam.org>
2020-06-07 22:37:12 -0400

    Add smooth transitions

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -2,11 +2,16 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import Band from 'rarwe/models/band';
 import { empty } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+import { dasherize } from '@ember/string';
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: '',
   isAddButtonDisabled: empty('newBandName'),
+
+  router: service(),
+
   addBand: action(function() {
     this.set('isAddingBand', true);
   }),
@@ -19,7 +24,12 @@ export default Controller.extend({
     event.preventDefault();
     let newBand = Band.create({ name: this.newBandName});
     this.model.pushObject(newBand);
-    this.set('newBandName', '')
+    this.setProperties({
+      newBandName: '',
+      isAddingBand: false
+    });
+    newBand.set('slug', dasherize(newBand.name));
+    this.router.transitionTo('bands.band.songs', newBand.slug)
   }),
 
 });
