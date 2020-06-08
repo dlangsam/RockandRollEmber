85111364d2f87b255fe47cea1943d628c55db6f7
Devorah Langsam <devorah@langsam.org>
2020-06-07 22:04:10 -0400

    Add ability to add more bands to list

---
+++ app/controllers/bands.js
@@ -0,0 +1,24 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import Band from 'rarwe/models/band';
+import { empty } from '@ember/object/computed';
+
+export default Controller.extend({
+  isAddingBand: false,
+  newBandName: '',
+  isAddButtonDisabled: empty('newBandName'),
+  addBand: action(function() {
+    this.set('isAddingBand', true);
+  }),
+
+  cancelAddBand: action(function() {
+    this.set('isAddingBand', false);
+  }),
+
+  saveBand: action(function() {
+    let newBand = Band.create({ name: this.newBandName});
+    this.model.pushObject(newBand);
+    this.set('newBandName', '')
+  }),
+
+});
