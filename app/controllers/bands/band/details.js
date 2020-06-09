ceca7dedb773418d0e938d5d6d9bc55debab4453
Devorah Langsam <devorah@langsam.org>
2020-06-09 10:12:52 -0400

    Add ability to edit detail and show warning when changing routes before saving

---
+++ app/controllers/bands/band/details.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller'; 
+import { action } from '@ember/object';
+export default Controller.extend(
+  { isEditing: false,
+    toggleIsEditing: action(function() { 
+      this.toggleProperty('isEditing');
+    }) 
+});
